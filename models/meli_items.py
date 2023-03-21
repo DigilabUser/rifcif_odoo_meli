5649e0933ed16adf00da99b5c8edf4db57c504a5
Deyvids <deyvidsalvino@gmail.com>
2023-03-21 01:33:52 -0500

    Agregando modelos de items

--- models/meli_items.py
+++ models/meli_items.py
@@ -15,9 +15,36 @@ _logger = logging.getLogger(__name__)
 
 class MercadolibreItems(models.Model):
     _name = 'meli.items'
-    _rec_name="id_items"
+    _rec_name="title"
 
-    id_items = fields.Char('id items')
+    # Vista tree
+
+    id_items = fields.Char('Id de item')
+    title = fields.Char('Titulo')
+    price = fields.Char('Precio')
+    inventory_id = fields.Char('Id de inventario')
+    currency_id = fields.Char('Id de moneda')
+    initial_quantity = fields.Integer('Cantidad inicial')
+    available_quantity = fields.Integer('Cantidad disponible')
+    sold_quantity = fields.Integer('Cantidad vendidad')
+    logistic_type = fields.Char('Tipo de logistica')
+
+    # Vista Form
+
+    category_id = fields.Char('Id de categoria')
+    base_price = fields.Char('Precio base')
+    condition= fields.Char('Condición del item')
+    warranty = fields.Char('Tipo de garantia') # Preguntar
+    permalink = fields.Char('Link permanente')
+    pictures = fields.Char('Imagen') # Preguntar
+    geolocation = fields.Char('Localización')
+    author = fields.Char('Autor') # Preguntar
+    book_cover = fields.Char('Tipo de tapa')
+    book_genre = fields.Char('Género del libro')
+    isbn = fields.Char('Codigo de barra')
+    language = fields.Char('Idioma')
+    max_recommended_age = fields.Char('Edad maxima recomendada')
+    tags = fields.Char('Tags')
 
     def create_items_from_meli_items(self):
         pass
